1693285bb3b2cf623c041bfe216807bde95c4225
Dror Matalon <dror@matal.com>
2023-01-02 11:56:30 -0800

    Support content file

--- custom-webpack.config.ts
+++ custom-webpack.config.ts
@@ -1,5 +1,8 @@
 import type { Configuration } from 'webpack';
 
 module.exports = {
-  entry: { background: { import: 'src/background.ts', runtime: false } },
+  entry: {
+    background: { import: 'src/background.ts', runtime: false },
+    content: { import: 'src/content.ts', runtime: false },
+  },
 } as Configuration;
