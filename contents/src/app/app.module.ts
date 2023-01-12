27495423f12cea84571e32ef8fadb9b2bab769dd
Dror Matalon <dror@matal.com>
2023-01-11 18:01:28 -0800

    Added with latest needed modules

--- contents/src/app/app.module.ts
+++ contents/src/app/app.module.ts
@@ -7,16 +7,23 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
 import { DragDropModule } from '@angular/cdk/drag-drop';
 
 import { LoggerModule, NgxLoggerLevel } from 'ngx-logger';
+import { NgxWebstorageModule } from 'ngx-webstorage';
 
 import { createCustomElement } from '@angular/elements';
 
 import { LeahContentComponent } from './leah-content/leah-content.component';
+import { SettingsComponent } from './settings/settings.component';
 
 @NgModule({
-  declarations: [AppComponent, LeahContentComponent],
+  declarations: [AppComponent, LeahContentComponent, SettingsComponent],
   imports: [
     BrowserModule,
     MatButtonToggleModule,
@@ -25,11 +32,17 @@ import { LeahContentComponent } from './leah-content/leah-content.component';
     MatIconModule,
     MatSliderModule,
     MatGridListModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatSlideToggleModule,
+    MatSelectModule,
     DragDropModule,
+    NgxWebstorageModule.forRoot(),
     LoggerModule.forRoot({
       level: NgxLoggerLevel.INFO,
     }),
   ],
+  exports: [MatFormFieldModule],
   providers: [],
   bootstrap: [AppComponent],
 })
