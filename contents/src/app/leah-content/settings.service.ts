973502c5aac73b80f204cc27f89b57959df8061e
Dror Matalon <dror@matal.com>
2023-01-18 22:40:45 -0800

    Migrate to ../settings

--- contents/src/app/leah-content/settings.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@angular/core';
-
-@Injectable({
-  providedIn: 'root',
-})
-export class SettingsService {
-  public static LISTEN_TIMEOUT = 3; // Number of seconds of silence before we respond
-}
