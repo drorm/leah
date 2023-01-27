4b9138180039ec101cc91c03e8d7a0205256cf71
Dror Matalon <dror@matal.com>
2023-01-26 17:33:45 -0800

    Removed original Angular files that aren't used anymore.

--- custom-webpack.config.ts
+++
@@ -1,8 +0,0 @@
-import type { Configuration } from 'webpack';
-
-module.exports = {
-  entry: {
-    background: { import: 'src/background.ts', runtime: false },
-    content: { import: 'src/content.ts', runtime: false },
-  },
-} as Configuration;
