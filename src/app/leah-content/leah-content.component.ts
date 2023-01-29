c0c0ce8bbfa0fe71d86e678c6d7f5efb07f92849
Dror Matalon <dror@matal.com>
2023-01-29 13:36:39 -0800

    update the status to WAITING when not listening.

--- src/app/leah-content/leah-content.component.ts
+++ src/app/leah-content/leah-content.component.ts
@@ -9,6 +9,8 @@ import { SettingsService } from '../settings/settings.service';
 import { MatDialog } from '@angular/material/dialog';
 import { MatCardModule } from '@angular/material/card';
 
+const WAITING = 'press the red button to start';
+
 @Component({
   selector: 'custom-leah-content',
   templateUrl: './leah-content.component.html',
@@ -30,7 +32,7 @@ export class LeahContentComponent {
   currentPrompt: any = null;
   dictation: any; // Current dictation
   voices: any;
-  status = 'Press the red button to start';
+  status = WAITING;
   userSettings: any;
 
   constructor(
@@ -69,6 +71,7 @@ export class LeahContentComponent {
         // This will also catch 'No speech was detected, try again' which is not really an error
         this.logger.error(err);
         this.listening = false;
+        this.setStatus(WAITING);
         alert(err);
         return;
       }
@@ -135,6 +138,7 @@ export class LeahContentComponent {
     this.conversing = false;
     this.listening = false;
     await this.voiceService.stop();
+    this.setStatus(WAITING);
   }
 
   settings() {
