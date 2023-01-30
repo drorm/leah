09d4b6b2c682c8671e3a2b5f9292807b3725de8e
Dror Matalon <dror@matal.com>
2023-01-29 21:39:32 -0800

    Put the code in a function and wait till the page is loaded to inject it. Hopefully this should solve the following that showed up periodically: - _app-cca043450d0dc72f.js:7 Error: NG0908     at new ye (main.js:1:99031)     at v0 (main.js:1:106040)     at n.bootstrapModuleFactory (main.js:1:106052)     at main.js:1:107160 which is not documented anywhere but seems to do with zone.js not being loaded.

--- src/content.ts
+++ src/content.ts
@@ -13,44 +13,53 @@
  * which will load the content Angular essentials app
  */
 // Inject the scripts into the page
-let scr = document.createElement('script');
-scr.type = 'text/javascript';
-scr.src = chrome.runtime.getURL('runtime.js');
-try {
-  (document.head || document.documentElement).appendChild(scr);
-} catch (e) {
-  console.log(e);
+//
+
+function inject() {
+  let scr = document.createElement('script');
+  scr.type = 'text/javascript';
+  scr.src = chrome.runtime.getURL('runtime.js');
+  try {
+    (document.head || document.documentElement).appendChild(scr);
+  } catch (e) {
+    console.log(e);
+  }
+
+  // Lazy copy and paste, but clearer
+  scr = document.createElement('script');
+  scr.type = 'text/javascript';
+  scr.src = chrome.runtime.getURL('polyfills.js');
+  try {
+    (document.head || document.documentElement).appendChild(scr);
+  } catch (e) {
+    console.log(e);
+  }
+
+  scr = document.createElement('script');
+  scr.type = 'text/javascript';
+  scr.src = chrome.runtime.getURL('main.js');
+  try {
+    (document.head || document.documentElement).appendChild(scr);
+  } catch (e) {
+    console.log(e);
+  }
+
+  // Inject the CSS into the page
+  var link = document.createElement('link');
+  link.rel = 'stylesheet';
+  link.type = 'text/css';
+  link.href = chrome.runtime.getURL('styles.css');
+  document.head.appendChild(link);
+  const html3 = `
+  <custom-root> Custom</custom-root>
+  <leah-content>leah</leah-content>
+  `;
+  const div = document.createElement('div');
+  div.innerHTML = html3;
+  document.body.insertBefore(div, document.body.firstChild);
 }
 
-// Lazy copy and paste, but clearer
-scr = document.createElement('script');
-scr.type = 'text/javascript';
-scr.src = chrome.runtime.getURL('polyfills.js');
-try {
-  (document.head || document.documentElement).appendChild(scr);
-} catch (e) {
-  console.log(e);
-}
-
-scr = document.createElement('script');
-scr.type = 'text/javascript';
-scr.src = chrome.runtime.getURL('main.js');
-try {
-  (document.head || document.documentElement).appendChild(scr);
-} catch (e) {
-  console.log(e);
-}
-
-// Inject the CSS into the page
-var link = document.createElement('link');
-link.rel = 'stylesheet';
-link.type = 'text/css';
-link.href = chrome.runtime.getURL('styles.css');
-document.head.appendChild(link);
-const html3 = `
-    <custom-root> Custom</custom-root>
-    <leah-content>leah</leah-content>
-`;
-const div = document.createElement('div');
-div.innerHTML = html3;
-document.body.insertBefore(div, document.body.firstChild);
+// Inject the scripts into the page once it's loaded
+document.addEventListener('DOMContentLoaded', function (event) {
+  inject();
+});
