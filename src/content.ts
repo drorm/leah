d040dee2f3a525f7c964f1c8e0ffef8afd7ac6ff
Dror Matalon <dror@matal.com>
2023-01-30 21:58:46 -0800

    Undo wait till page load to inject. Makes the issue with zone.js worse.

--- src/content.ts
+++ src/content.ts
@@ -13,53 +13,51 @@
  * which will load the content Angular essentials app
  */
 // Inject the scripts into the page
-//
-
-function inject() {
-  let scr = document.createElement('script');
-  scr.type = 'text/javascript';
-  scr.src = chrome.runtime.getURL('runtime.js');
-  try {
-    (document.head || document.documentElement).appendChild(scr);
-  } catch (e) {
-    console.log(e);
-  }
-
-  // Lazy copy and paste, but clearer
-  scr = document.createElement('script');
-  scr.type = 'text/javascript';
-  scr.src = chrome.runtime.getURL('polyfills.js');
-  try {
-    (document.head || document.documentElement).appendChild(scr);
-  } catch (e) {
-    console.log(e);
-  }
-
-  scr = document.createElement('script');
-  scr.type = 'text/javascript';
-  scr.src = chrome.runtime.getURL('main.js');
-  try {
-    (document.head || document.documentElement).appendChild(scr);
-  } catch (e) {
-    console.log(e);
-  }
-
-  // Inject the CSS into the page
-  var link = document.createElement('link');
-  link.rel = 'stylesheet';
-  link.type = 'text/css';
-  link.href = chrome.runtime.getURL('styles.css');
-  document.head.appendChild(link);
-  const html3 = `
-  <custom-root> Custom</custom-root>
-  <leah-content>leah</leah-content>
-  `;
-  const div = document.createElement('div');
-  div.innerHTML = html3;
-  document.body.insertBefore(div, document.body.firstChild);
+let scr = document.createElement('script');
+scr.type = 'text/javascript';
+scr.src = chrome.runtime.getURL('runtime.js');
+try {
+  (document.head || document.documentElement).appendChild(scr);
+  console.error('runtime.js injected');
+} catch (e) {
+  console.error('error in loading runtime.js');
+  console.error(e);
 }
 
-// Inject the scripts into the page once it's loaded
-document.addEventListener('DOMContentLoaded', function (event) {
-  inject();
-});
+// Lazy copy and paste, but clearer
+scr = document.createElement('script');
+scr.type = 'text/javascript';
+scr.src = chrome.runtime.getURL('polyfills.js');
+try {
+  (document.head || document.documentElement).appendChild(scr);
+  console.error('polyfills.js injected');
+} catch (e) {
+  console.error('error in loading polyfills.js');
+  console.error(e);
+}
+
+scr = document.createElement('script');
+scr.type = 'text/javascript';
+scr.src = chrome.runtime.getURL('main.js');
+try {
+  (document.head || document.documentElement).appendChild(scr);
+  console.error('main.js injected');
+} catch (e) {
+  console.error('error in loading main.js');
+  console.error(e);
+}
+
+// Inject the CSS into the page
+var link = document.createElement('link');
+link.rel = 'stylesheet';
+link.type = 'text/css';
+link.href = chrome.runtime.getURL('styles.css');
+document.head.appendChild(link);
+const html3 = `
+    <custom-root> Custom</custom-root>
+    <leah-content>leah</leah-content>
+`;
+const div = document.createElement('div');
+div.innerHTML = html3;
+document.body.insertBefore(div, document.body.firstChild);
+console.error('content.ts injected');
