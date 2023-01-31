b655c871fecf2c01a79881c99837884429e03d14
Dror Matalon <dror@matal.com>
2023-01-31 12:43:24 -0800

    Initial version of create prompt dialog

--- src/app/prompts/prompts.component.ts
+++ src/app/prompts/prompts.component.ts
@@ -3,6 +3,8 @@ import { MatTableModule } from '@angular/material/table';
 import { DataSource } from '@angular/cdk/table';
 import { SettingsService } from '../settings/settings.service';
 import { NGXLogger, NgxLoggerLevel } from 'ngx-logger';
+import { MatDialog } from '@angular/material/dialog';
+import { CreatePromptDialog } from './create-prompt-dialog';
 
 export interface PeriodicElement {
   name: string;
@@ -30,9 +32,31 @@ export class PromptsComponent {
 
   constructor(
     private settingsService: SettingsService,
+    private dialog: MatDialog,
     private logger: NGXLogger
   ) {
     this.logger.trace('PromptsComponent constructor');
     this.logger.info('PromptsComponent', this.prompts);
+  }
+
+  add() {
+    this.logger.trace('PromptsComponent add');
+    // open a dialog to create a new prompt
+    const dialogRef = this.dialog.open(CreatePromptDialog, {
+      data: {
+        type: '',
+        title: '',
+        body: '',
+        listenVoice: '',
+        speakVoice: '',
+      },
+    });
+    dialogRef.afterClosed().subscribe((result) => {
+      this.logger.trace('The dialog was closed', result);
+      if (result) {
+        this.prompts.push(result);
+        this.logger.info('PromptsComponent', this.prompts);
+      }
+    });
   }
 }
